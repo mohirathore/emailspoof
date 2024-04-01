719cdca0f950d3dd3f82fa083a3da21c80b70beb
mohirathore <151892032+mohirathore@users.noreply.github.com>
2024-04-01 14:38:20 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,1 +1,18 @@
+<?php
+$to = "recipient@example.com";
+$subject = "Test Email";
+$message = "This is a test email.";
+$sender_email = "sender@example.com";
+$sender_name = "Your Name";
+
+$headers = "From: $sender_name <$sender_email>\r\n";
+$headers .= "Reply-To: $sender_name <$sender_email>\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Email sending failed.";
+}
+?>
 
